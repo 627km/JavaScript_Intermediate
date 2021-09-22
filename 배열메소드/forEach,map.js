19848cd0c2d253a7bc54d4809f4318711b2db053
627km <86594128+627km@users.noreply.github.com>
2021-09-22 19:35:53 +0900

    Update forEach,map.js

--- 배열메소드/forEach,map.js
+++ 배열메소드/forEach,map.js
@@ -23,3 +23,42 @@ number.reduce((acc, el, i, arr) => {
   return nextAccValue;
 }, initialAccValue);  
 // acc (accumulator: 누산기)
+
+/**
+ * sort 메소드
+ * : sort 메소드에 아무런 아규먼트도 전달하지 않을 때는 기본적으로 유니코드에 정의된 문자열 순서에 따라 정렬된다. 
+ */
+const letters = ['D', 'C', 'E', 'B', 'A'];
+const numbers = [1, 10, 4, 21, 36000];
+
+letters.sort(); 
+numbers.sort();
+
+console.log(letters); // (5) ["A", "B", "C", "D", "E"]
+console.log(numbers); // (5) [1, 10, 21, 36000, 4] -> 숫자를 정렬할 때는 우리가 상식적으로 이해하는 오름차순이나 내림차순 정렬이 되지않는다. 
+
+// 오름차순 정렬
+numbers.sort((a, b) => a - b);
+console.log(numbers); // (5) [1, 4, 10, 21, 36000] 
+
+// 내림차순 정렬
+numbers.sort((a, b) => b - a);
+console.log(numbers); // (5) [36000, 21, 10, 4, 1] 
+
+// sort 메소드를 사용할 때 한 가지 주의해야 할 부분은 메소드를 실행하는 원본 배열의 요소들을 정렬한다는 점이다. 그래서 한 번 정렬하고 나면 정렬하기 전의 순서로 다시 되돌릴 수 없다. 
+
+/**
+ * revers 메소드
+ * : 배열의 순서를 뒤집는 메소드이다.
+ * : 별도의 파라미터가 존재하지 않기 때문에 단순히 메소드를 호출해주기만 하면 배열의 순서가 뒤집힌다. 
+ * : sort 메소드와 마찬가지로 원본 배열의 요소들을 뒤집어 버린다는 점이다. 
+ */
+
+const letters = ['a', 'c', 'b'];
+const numbers = [421, 721, 353];
+
+letters.reverse();
+numbers.reverse();
+
+console.log(letters); // (3) ["b", "c", "a"]
+console.log(numbers); // (3) [353, 721, 421]

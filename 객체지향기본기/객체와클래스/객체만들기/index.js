bbe46f34ede90cc1f7d22fcb77259818bdad62cc
627km <86594128+627km@users.noreply.github.com>
2021-09-29 23:11:26 +0900

    Add files via upload

---
+++ 객체지향기본기/객체와클래스/객체만들기/index.js
@@ -0,0 +1,17 @@
+const user = {
+  email: 'rudals_jeon@naver.com', // 객체의 상태를 나타내는 변수 : 프로퍼티
+  birthdate: '1998-06-27',  // 객체의 상태를 나타내는 변수 : 프로퍼티
+  buy(item) {  // 객체의 행동을 나타내는 함수 : 메소드
+    console.log(`${this.email} buys ${item.name}`);  // this : 현재의 객체를 나타낸다. 
+  },
+};
+// 프로퍼티와 메소드를 나열하는 것을 Obejct Literal(객체를 나타내는 문자열) 이라고 한다. 
+
+const item = {
+  name: '스웨터',
+  price: 30000,
+}; 
+
+console.log(user.email);
+console.log(user.birthdate);
+user.buy(item);

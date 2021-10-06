3b5a55a186ac863986e8a865407f218ba1f94117
627km <86594128+627km@users.noreply.github.com>
2021-10-06 23:56:49 +0900

    Update index.js

--- OOP_basic/객체와클래스/객체만들기4/index.js
+++ OOP_basic/객체와클래스/객체만들기4/index.js
@@ -1,5 +1,5 @@
 // 클래스 (class)
-// 보통 클래스에서는 constructor 안에 프로퍼티를 , 그 밖에 메소드를 정의한다. 
+// 보통 클래스에서는 constructor 안에 프로퍼티를 정의하고 메소드는 그 밖에다 정의한다. 
 class User {  
   constructor(email, birthdate) { // constructor (생성자 메소드)
     this.email = email;  // this (생성되는 객체를 가리킴)

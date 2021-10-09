ef2e8ba222df9c09f2a43783c54cb899a9cee85d
627km <rudals_jeon@naver.com>
2021-10-09 23:55:01 +0900

    add

---
+++ OOP_basic/OOP/inheritance/index.js
@@ -0,0 +1,29 @@
+// 상속 (inheritance)
+class User {
+  constructor(email, birthdate) {
+    this.email = email;
+    this.birthdate = birthdate;
+  }
+
+  buy(item) {
+    console.log(`${this.email} buys ${item.name}`);
+  }
+}
+
+class PremiumUser extends User {  // extends User -> User class 를 상속한다. 
+  constructor(email, birthdate, level) {
+  // 상속받은 User class 와 겹치는 부분은 입력하지 않아도 된다. 
+  //  this.email = email;
+  //  this.birthdate = birthdate;
+    this.level = level;
+  }
+// 상속받은 User class 와 겹치는 부분은 입력하지 않아도 된다.
+//  buy(item) {
+//    console.log(`${this.email} buys ${item.name}`);
+//  }
+
+  streamMusicForFree() {
+    console.log(`Free music streaming for ${this.email}`);
+  }
+}
+

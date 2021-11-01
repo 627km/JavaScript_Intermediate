f794107ff0319230e73d9dfbbbe370b811662882
627km <rudals_jeon@naver.com>
2021-11-01 23:54:38 +0900

    update

--- OOP_basic/OOP/inheritance/inheritance.js
+++ OOP_basic/OOP/inheritance/inheritance.js
@@ -10,9 +10,9 @@ class User {
   }
 }
 
-class PremiumUser extends User {  // extends User -> User class 를 상속한다. 
+class PremiumUser extends User {  // extends User -> User class에 있는 email, birthdate 프로퍼티와 buy 메소드를 상속받는다. 
   constructor(email, birthdate, level) {
-  // 상속받은 User class 와 겹치는 부분은 입력하지 않아도 된다. 
+  // 상속받은 User class 와 겹치는 부분은 입력하지 않아도 된다. (똑같은 코드를 또다시 작성하지 않아도된다. '코드의 재사용성이 좋아진다.')
   //  this.email = email;
   //  this.birthdate = birthdate;
     super(email, birthday, level); // 자식 class 생성자 함수 안에서 부모 class의 생성사 함수를 super 라는 키워드로 반드시 호출해야한다.(에러발생 x)

733b1662a2a9e623fcbcc4fe24a853d10060b39f
627km <rudals_jeon@naver.com>
2021-11-08 23:54:04 +0900

    add

---
+++ webDevelopment_basic/webAPI/method/method.js
@@ -0,0 +1,9 @@
+// 메소드 (method)
+
+/**
+ * 요청(request)의 종류 - 메소드 
+ * 1. 데이터 조회 - Get -> Get Request
+ * 2. 데이터 추가 - Post -> Post Request
+ * 3. 데이터 수정 - Put -> Put Request
+ * 4. 데이터 삭제 - Delete -> Delete Request
+ */

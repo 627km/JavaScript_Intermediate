68e15690c387c999ae5aad53f5d90cc0aacb8d13
627km <rudals_jeon@naver.com>
2021-11-09 23:33:31 +0900

    add

--- webDevelopment_basic/webAPI/method,request/method,request.js
+++ webDevelopment_basic/webAPI/method,request/method,request.js
@@ -1,9 +1,14 @@
 // 메소드 (method)
-
 /**
  * 요청(request)의 종류 - 메소드 
  * 1. 데이터 조회 - Get -> Get Request
  * 2. 데이터 추가 - Post -> Post Request
  * 3. 데이터 수정 - Put -> Put Request
  * 4. 데이터 삭제 - Delete -> Delete Request
+ */
+
+/**
+ * Request
+ * - Head : request에 대한 부가 정보를 담고 있음 (method)
+ * - body : 실제 데이터를 담는 부분 (get, delete 메소드는 body가 업다.)
  */

c5a923f2be8b5192172aa13767f5c1804841be6b
627km <rudals_jeon@naver.com>
2021-11-11 23:39:44 +0900

    add

---
+++ webDevelopment_basic/webAPI/API/API.js
@@ -0,0 +1,23 @@
+/**
+ * API(Application Programming Interface)
+ * : 개발할 때 사용할 수 있도록 특정 라이블러리나 플랫폼 등이 제공하는 데이터나 함수 등을 의미
+ * 
+ * 어느 URL로 어떤 리퀘스트를 보냈을 때, 무슨 처리가 수행되고 어떤 리스폰스가 오는지에 관해 미리 정해진 규격을 Web API 라고 한다. 
+ *  */ 
+
+/**
+ * Rest API
+ * : 오늘날 많은 웹 개발자들이 Web API 설계할 때, 준수하기 위해 노력하는 일종의 가이드라인이다.
+ * 
+ * REST Architecture
+ * : 미국의 컴퓨터 과학자인 Roy Fielding이 본인의 박사논문에서 제시한 개념인데, 그는 웹이 갖추어야 할 이상적인 아키텍처(구조)로 REST architecture라는 개념을 제시했다.
+ *   REST는 Representational State Transfer(표현적인 상태 이전)의 줄임말로, 해석하면 '표현적인, 상태 이전'이라는 뜻이다. 
+ * 
+ * REST architecture이 되기위한 조건
+ * 1. Client-Server
+ * 2. Stateless
+ * 3. Cache
+ * 4. Uniform Interface
+ * 5. Layered System
+ * 6. Code on Demand
+ */

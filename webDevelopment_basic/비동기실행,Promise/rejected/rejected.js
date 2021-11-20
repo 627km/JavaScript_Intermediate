248a199ec7c6e34c1b123585661e87cf22f9e90b
627km <rudals_jeon@naver.com>
2021-11-20 23:14:39 +0900

    add

---
+++ webDevelopment_basic/비동기실행,Promise/rejected/rejected.js
@@ -0,0 +1,8 @@
+// rejected 상태일때 실행되는 콜백 
+fetch('https://jsonplaceholder.typicode.com/users')
+  .then((response) => response.text(), (error) => { console.log(error); }) // 첫 번째 콜백 : fulfilled 상태일때 실행할 콜백 , 두 번째 콜백 : rejected 상태일때 실행할 콜백
+  .then((result) => { console.log(result); });
+
+// 만약 인터넷이 끊긴 상태에서 위의 코드를 실행시키면 
+// VM253:1 GET https://jsonplaceholder.typicode.com/users net::ERR_INTERNET_DISCONNECTED 200
+// 라는 작업실패정보가 출력된다. 

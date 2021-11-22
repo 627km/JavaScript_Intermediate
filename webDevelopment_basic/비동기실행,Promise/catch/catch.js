ba2068387c833a1205eed0fdfa4f5e016c172ab5
627km <rudals_jeon@naver.com>
2021-11-22 21:36:05 +0900

    add

--- webDevelopment_basic/비동기실행,Promise/catch/catch.js
+++ webDevelopment_basic/비동기실행,Promise/catch/catch.js
@@ -11,3 +11,21 @@ fetch('https://jsonplaceholder.typicode.com/users')
   .catch((error) => { console.log(error); })  // 첫 번째 then 메소드에 있는 두 번째 콜백을 catch 메소드안에 넣었다.
 //.then(undefined, (error) => { console.log(error); }) 와 똑같은 의미를 갖는다. (catch 메소드는 then 메소드를 변형한 것에 불과하기 때문)
   .then((result) => { console.log(result); });
+
+
+// 실무에서 사용하는 catch 메소드: catch 메소드를 마지막에 적어준다. 
+fetch('https://jsonplaceholder.typicode.com/users')
+  .then((response) => response.text())
+  .catch((error) => { console.log(error); })
+  .then((result) => { 
+    console.log(result);
+    throw new Error('test');  // 이렇게 catch 메소드 뒤에 발생하는 error는 잡아내지 못한다. 
+  })
+
+fetch('https://jsonplaceholder.typicode.com/users')
+  .then((response) => response.text())
+  .then((result) => { 
+    console.log(result);
+    throw new Error('test');
+  })
+  .catch((error) => { console.log(error); })  // 이렇게 catch 메소드를 맨 마지막에 적어주면 error를 잡아낼 수 있다. 
---
+++ webDevelopment_basic/비동기실행,Promise/finally/finally.js
@@ -0,0 +1,10 @@
+// finally 메소드 - 항상 실행하고 싶은 콜백이 있을 경우 finally 메소드에 넣으면 된다. 
+fetch('https://jsonplaceholder.typicode.com/users')
+  .then((response) => response.text())
+  .then((result) => { console.log(result); })
+  .catch((error) => { 
+    console.log(error);
+    throw new Error('from catch method');
+  })
+  .finally(() => { console.log('exit'); } ); // 보통 catch 메소드 뒤에 적는다. 
+// Error가 발생하더라도 finally 메소드는 실행이 된다. 
